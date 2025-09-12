36f4a083c65ddf17b4afac44ee9cb0d1cab65668
lucaasdiez <lucaasdiez@gmail.com>
2025-09-12 18:06:30 -0300

    Subiendo proyecto Spring Boot inicial

---
+++ src/test/java/com/clubNautico/ClubNauticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubNautico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
