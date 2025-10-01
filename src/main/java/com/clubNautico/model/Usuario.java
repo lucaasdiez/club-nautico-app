3bb8c004f2de74e5105fc9c9b6f86f0cf2468a7c
lucaasdiez <lucaasdiez@gmail.com>
2025-10-01 11:59:16 -0300

    Agregando carpetas

---
+++ src/main/java/com/clubNautico/model/Usuario.java
@@ -0,0 +1,23 @@
+package com.clubNautico.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public abstract class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
